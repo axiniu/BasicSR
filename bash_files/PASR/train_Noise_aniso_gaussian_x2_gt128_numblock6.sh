0c80bc89e57e95cd904dcfe9d76370c75f6a3668
zk <pantehonzak5100@gmail.com>
2022-05-06 11:48:47 +0900

    bashfiles

---
+++ bash_files/PASR/train_Noise_aniso_gaussian_x2_gt128_numblock6.sh
@@ -0,0 +1,2 @@
+CUDA_VISIBLE_DEVICES=0 \
+python basicsr/train.py -opt options/train/PASR/train_Noise_aniso_gaussian_x2.yml
---
+++ bash_files/PASR/train_noNoise_iso_gaussian_x2.sh
@@ -0,0 +1,2 @@
+CUDA_VISIBLE_DEVICES=4 \
+python basicsr/train.py -opt options/train/PASR/train_noNoise_iso_gaussian_x2.yml
