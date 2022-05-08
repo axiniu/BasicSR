9f71592343c62b89ee31e940048525439bbca451
zk <pantehonzak5100@gmail.com>
2022-05-08 17:59:18 +0900

    yml & bashfiles

---
+++ bash_files/PASR/train_Noise_aniso_gaussian_x4_gt128_batchsize64.sh
@@ -0,0 +1,2 @@
+CUDA_VISIBLE_DEVICES=3 \
+python basicsr/train.py -opt options/train/PASR/train_Noise_aniso_gaussian_x4.yml
--- scripts/data_preparation/generate_degrade_img.py
+++ scripts/data_preparation/generate_degrade_img.py
@@ -7,42 +7,48 @@ from basicsr.utils import SRMDPreprocessing
 
 # 在代码修改开头的数据配置，然后代码根目录，执行 python scripts/data_preparation/generate_degrade_img.py
 
-root_path = './datasets/sr' # 修改这个目录，指定为数据的根目录，可以用 ln -s 指令和代码目录的datasets文件夹关联
-dataset_names = ['Urban100'] # div2k set5 set14 B100 Urban100 可以处理的数据集
+#root_path = './datasets/sr' # 修改这个目录，指定为数据的根目录，可以用 ln -s 指令和代码目录的datasets文件夹关联
+
+#dataset_name = 'Set5'
+root_path = '/workspace/hdd/datasets'
+dataset_names = ['Urban100', 'Set5', 'Set14', 'BSD'] # div2k set5 set14 B100 Urban100 可以处理的数据集
 scales = [2,3,4] # 不同大小的scale
 kernel_widths = [0,0.6,1.2,1.8] # 不同的同向高斯核宽度
 blur_types = ['iso_gaussian']  # iso_gaussian or aniso_gaussian，高斯核类型，aniso_gaussian还未处理
 
 for dataset_name in dataset_names:
+    print(dataset_name)
+    #import pdb;pdb.set_trace()
     if dataset_name == 'div2k':
         path_root_div2k = os.path.join(root_path,'DIV2K')
         hr_names = glob.glob(os.path.join(path_root_div2k,'DIV2K_train_HR','0[89]*.png'))
         hr_names.sort()
         hr_names = hr_names[1:]
         save_path_root = os.path.join(path_root_div2k, 'DIV2K_train_HR_with_degrade')
-    elif dataset_name == 'set5':
-        path_root_set5 = os.path.join(root_path, 'Classical/Set5/')
-        hr_names = glob.glob(os.path.join(path_root_set5, 'GTmod12', '*.png'))
+    elif dataset_name == 'Set5':
+        path_root_set5 = os.path.join(root_path, 'super_resolution/Set5/')#
+        hr_names = glob.glob(os.path.join(path_root_set5, 'HR', '*.png'))
         hr_names.sort()
-        save_path_root = os.path.join(path_root_set5, 'GTmod12_with_degrade')
-    elif dataset_name == 'set14':
-        path_root_set14 = os.path.join(root_path, 'Classical/Set14/')
-        hr_names = glob.glob(os.path.join(path_root_set14,'GTmod12','*.png'))
+        save_path_root = os.path.join(path_root_set5, 'HR_with_degrade')
+    elif dataset_name == 'Set14':
+        path_root_set14 = os.path.join(root_path, 'super_resolution/Set14/')
+        hr_names = glob.glob(os.path.join(path_root_set14,'HR','*.png'))
         hr_names.sort()
-        save_path_root = os.path.join(path_root_set14, 'GTmod12_with_degrade')
-    elif dataset_name == 'B100':
-        path_root_b100 = os.path.join(root_path, 'Classical/BSDS100/')
-        hr_names = glob.glob(os.path.join(path_root_b100, '*.png'))
+        save_path_root = os.path.join(path_root_set14, 'HR_with_degrade')
+    elif dataset_name == 'BSD':
+        path_root_b100 = os.path.join(root_path, 'super_resolution/BSD/')#/mnt/hdd/zhangkang/datasets/super_resolution/BSD
+        hr_names = glob.glob(os.path.join(path_root_b100, 'HR', '*.png'))
         hr_names.sort()
-        save_path_root = os.path.join(path_root_b100, 'BSDS100_with_degrade')
+        save_path_root = os.path.join(path_root_b100, 'HR_with_degrade')
     elif dataset_name == 'Urban100':
-        path_root_urban100 = os.path.join(root_path, 'Classical/urban100/')
-        hr_names = glob.glob(os.path.join(path_root_urban100, '*.png'))
+        path_root_urban100 = os.path.join(root_path, 'super_resolution/urban100/')
+        hr_names = glob.glob(os.path.join(path_root_urban100, 'HR', '*.png'))
         hr_names.sort()
-        save_path_root = os.path.join(path_root_urban100, 'urban100_with_degrade')
+        save_path_root = os.path.join(path_root_urban100, 'HR_with_degrade')
     else:
         raise TypeError
 
+    # import pdb;pdb.set_trace()
     os.makedirs(save_path_root, exist_ok=True)
     mode = 'bicubic'
     sig_min = 0.2  # training 0.2 for x2, 0.2 for x3, 0.2 for x4 for iso_gaussian
@@ -92,5 +98,36 @@ for dataset_name in dataset_names:
             elif blur_type == 'aniso_gaussian':
                 kernel_size = 11  # gaussian kernel size
                 degrade_type = '{}_lambda1_{}_lambda2_{}_theta_{}'.format(blur_type,lambda_1,lambda_2,theta)
+
+                save_path = os.path.join(save_path_root, 'X{}'.format(scale), degrade_type)
+                os.makedirs(save_path, exist_ok=True)
+
+                degrade = SRMDPreprocessing(
+                    scale=scale,
+                    mode=mode,
+                    kernel_size=kernel_size,
+                    blur_type=blur_type,
+                    sig=sig,
+                    sig_min=sig_min,
+                    sig_max=sig_max,
+                    lambda_1=lambda_1,
+                    lambda_2=lambda_2,
+                    theta=theta,
+                    lambda_min=lambda_min,
+                    lambda_max=lambda_max,
+                    noise=noise
+                )
+
+                for name in hr_names:
+                    hr = Image.open(name)
+                    hr = torchvision.transforms.ToTensor()(hr)
+                    hr = hr.unsqueeze(0).cuda()
+                    lr, _ = degrade(hr, random=False)
+                    print(hr.shape,lr.shape)
+                    lr = lr.squeeze(0).cpu()
+                    lr_img = torchvision.transforms.ToPILImage()(lr)
+                    lr_img.save(os.path.join(save_path, os.path.basename(name)))
+
+
             else:
                 raise TypeError
